7e5017f3989487f5ffea590ec1cb601804088ddf
proslin <linchik@mail.ru>
2021-02-26 16:08:31 +0300

    3_4

--- Правильное использование пользовательских функций/3_4.js
+++ Правильное использование пользовательских функций/3_4.js
@@ -1,4 +1,12 @@
 function getDigitsSum(num) {
-    digitArr = String(num).split('');
-    for (i = 0; i <  digitArr.length;)
+    let digitArr = String(num).split('');
+    let sum=0;
+    for (let i = 0; i <  digitArr.length; i++) {
+      sum+=Number(digitArr[i]);
+    }
+    return sum;
 } 
+
+for (let i = 1; i<=2021; i++) {
+       if (getDigitsSum(i) == 13) console.log(getDigitsSum(i) + ' '+i);
+}
